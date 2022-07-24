2a2c5e19f7a02af1227595122ba077f28a366d31
avcuenes <avcupyrz@gmail.com>
2022-07-24 22:36:52 +0300

    try some new algorithm

--- examples/applications/collision_avoidance/collision_avoidance.py
+++ examples/applications/collision_avoidance/collision_avoidance.py
@@ -6,48 +6,47 @@ import matplotlib.pyplot as plt
 class CollisionAvoidance():
     def __init__(self,number_of_waypoint):
         self.number_of_wp = number_of_waypoint
-        self.obstacle_x = [1,5,6,7,8]
-        self.obstacle_y = [1,5,6,7,8]
+        self.obstacle_x = [5]
+        self.obstacle_y = [5]
         self.number_of_obs = len(self.obstacle_x)
+        self.distance_between_wp = 2
+        self.distance_from_obstacle = 2
     
-    def Euclidean_distance(self,waypoint_one, waypoint_two):
-        return np.sqrt(pow(waypoint_one[0]-waypoint_two[0],2) + pow(waypoint_one[1]-waypoint_two[1],2))
+    def Euclidean_distance(self,point_one, point_two):
+        return np.sqrt(pow(point_one[0]-point_two[0],2) + pow(point_one[1]-point_two[1],2))
         
     def constr_one(self,x):
         """_summary_
-        This function constraint for problem 
+        This function is constraint function to use equal distance
 
         Args:
             x (list): _description_ waypoint list x and y
         """
         gx = []
-        """for i in range(0,self.number_of_wp-1):
-            gx.append((self.Euclidean_distance(waypoint_one=[x[2*i],x[2*i+1]],waypoint_two=[x[2*i+2],x[2*i+3]]) -50))
-            #print(i,"zzzz",np.sqrt((x[2*i] - x[2*i+2])**2 + (x[2*i+1] - x[2*i+3])**2), self.distance_to_target(x)/self.number_of_wp,gx)"""
-        g1 = 0
-        g2 = 0
-        g1 = pow((x[0]-x[2])**2+(x[1]-x[3]**2),0.5)
-        g1 =  g1-50
-        
-        g2 = pow((x[0]-x[2])**2+(x[1]-x[3]**2),0.5)
-        g2 = g2 - 50
-        gx.append(g1)
-        gx.append(g2)
-      
+        for i in range(0,self.number_of_wp-1):
+            gx.append((self.Euclidean_distance(point_one=[x[2*i],x[2*i+1]],point_two=[x[2*i+2],x[2*i+3]]) -self.distance_between_wp))
+            #print(i,"zzzz",np.sqrt((x[2*i] - x[2*i+2])**2 + (x[2*i+1] - x[2*i+3])**2), self.distance_to_target(x)/self.number_of_wp,gx)
         return gx
     
     def constr_two(self,x):
-        gx = 0
-        for i in range(0,self.number_of_wp):
+        """
+        This function use for obstacle avoidance constraint
+        
+        Args:
+            x (_type_): _description_ waypoint list x and y
+        """
+        gx = []
+        for i in range(1,self.number_of_wp-1):
             for j in range(0,self.number_of_obs):
-                gx += 1
-        pass
+                gx.append((self.Euclidean_distance(point_one=[x[2*i],x[2*i+1]],point_two=[self.obstacle_x[j],self.obstacle_y[j]]) -self.distance_from_obstacle))
+    
+        return gx
     
     def constr_tree(self,x):
         return pow((x[2]**2+x[3]**2),0.5)-10
     
     def constr_four(self,x):
-        return pow((x[4]-x[2])**2 +(x[5]-x[3])**2 ,0.5)-20
+        return pow((x[4]-x[2])**2 +(x[5]-x[3])**2 ,0.5)-50
     
     def violate(self,value):
             return 0 if value <= 0 else value
@@ -60,20 +59,25 @@ class CollisionAvoidance():
         
         x[0] = 0
         x[1] = 0
-        x[-1] = 100
-        x[-2] = 100
+        x[-1] = 10
+        x[-2] = 10
         
-        fx = self.Euclidean_distance(waypoint_one=[x[0],x[1]],waypoint_two=[x[2],x[3]])
-        print("first",fx)
+        fx = self.Euclidean_distance(point_one=[x[0],x[1]],point_two=[x[2],x[3]])        
+        fx += self.Euclidean_distance(point_one=[x[-1],x[-2]],point_two=[x[2],x[3]])
         
-        fx += self.Euclidean_distance(waypoint_one=[x[-1],x[-2]],waypoint_two=[x[2],x[3]])
-        print("second",fx)
-        """for i in range(1,self.number_of_wp-1):
-            fx += self.Euclidean_distance(waypoint_one=[x[2*i],x[2*i+1]],waypoint_two=[x[2*i+2],x[2*i+3]])"""
+        for i in range(1,self.number_of_wp-2):
+            fx += self.Euclidean_distance(point_one=[x[2*i],x[2*i+1]],point_two=[x[2*i+2],x[2*i+3]])
 
         
         #const1 = self.violate(self.constr_one(x))
-        fx += max([self.constr_four(x)])**2 
+        # this is static penalty function 
+        # link == > https://www.researchgate.net/profile/Oezguer-Yeniay/publication/228339797_Penalty_Function_Methods_for_Constrained_Optimization_with_Genetic_Algorithms/links/56d1ecda08ae85c8234ade07/Penalty-Function-Methods-for-Constrained-Optimization-with-Genetic-Algorithms.pdf
+        
+        """print("const1",self.constr_one(x))
+        print("<<<<<<<<<<<<")
+        print(self.constr_two(x))"""
+        print(self.Euclidean_distance(point_one=[x[2],x[3]],point_two=[self.obstacle_x[0],self.obstacle_y[0]]))
+        fx += max(self.constr_one(x))**2+max(self.constr_two(x))**2
         return fx
     
     def run(self):
@@ -84,11 +88,10 @@ class CollisionAvoidance():
         "minmax": "min",
         }
         
-        print(len(problem_dict1["lb"]))
-        print(len(problem_dict1["ub"]))
+        
         
         ## Run the algorithm
-        model1 = BaseWOA(problem_dict1, epoch=100  , pop_size=200)
+        model1 = BaseWOA(problem_dict1, epoch=100  , pop_size=500)
         best_position, best_fitness = model1.solve()
         print(f"Best solution: {best_position}, Best fitness: {best_fitness}")
         self.plot(best_position)
@@ -108,7 +111,7 @@ class CollisionAvoidance():
         print(x)
         print(y)         
         plt.scatter(x, y)
-        plt.scatter(self.obstacle_x,self.obstacle_y)
+        plt.scatter(self.obstacle_x,self.obstacle_y,s=(10 * self.distance_from_obstacle)**2 )
         plt.show()
         
         
--- mealpy/swarm_based/WOA.py
+++ mealpy/swarm_based/WOA.py
@@ -63,6 +63,7 @@ class BaseWOA(Optimizer):
             epoch (int): The current iteration
         """
         a = 2 - 2 * epoch / (self.epoch - 1)  # linearly decreased from 2 to 0
+        print("sss")
         pop_new = []
         for idx in range(0, self.pop_size):
             r = np.random.rand()
@@ -203,3 +204,97 @@ class HI_WOA(Optimizer):
             for idx_counter, idx in enumerate(idx_list):
                 self.pop[idx] = pop_child[idx_counter]
         self.nfe_per_epoch = nfe_epoch
+        
+        
+        
+        
+        
+
+class IWOA(Optimizer):
+    """
+    The original version of: Improved Whale Optimization Algorithm (IWOA)
+
+    Links:
+        1. https://downloads.hindawi.com/journals/ddns/2021/8832251.pdf
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.WOA import IWOA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "fit_func": fitness_function,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> model = IWOA(problem_dict1, epoch, pop_size)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Mirjalili, S. and Lewis, A., 2016. The whale optimization algorithm.
+    Advances in engineering software, 95, pp.51-67.
+    """
+
+    def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
+        """
+        Args:
+            problem (dict): The problem dictionary
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+        """
+        super().__init__(problem, kwargs)
+        self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.nfe_per_epoch = self.pop_size
+        self.sort_flag = False
+
+    def evolve(self, epoch):
+        """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
+        Args:
+            epoch (int): The current iteration
+        """
+        a = 2 - 2 * epoch / (self.epoch - 1)  # linearly decreased from 2 to 0
+        
+        if epoch < self.epoch*0.7:
+            a = pow(1-epoch/self.epoch)
+        else:
+            a = 2 - 2*epoch/self.epoch
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            r = np.random.rand()
+            A = 2 * a * r - a
+            C = 2 * r
+            l = np.random.uniform(-1, 1)
+            p = 0.5
+            b = 1
+            if np.random.uniform() < p:
+                if np.abs(A) < 1:
+                    D = np.abs(C * self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
+                    pos_new = self.g_best[self.ID_POS] - A * D
+                else:
+                    # x_rand = pop[np.random.np.random.randint(self.pop_size)]         # select random 1 position in pop
+                    x_rand = self.create_solution(self.problem.lb, self.problem.ub)
+                    D = np.abs(C * x_rand[self.ID_POS] - self.pop[idx][self.ID_POS])
+                    pos_new = x_rand[self.ID_POS] - A * D
+            else:
+                D1 = np.abs(self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
+                pos_new = self.g_best[self.ID_POS] + np.exp(b * l) * np.cos(2 * np.pi * l) * D1
+            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
+            pop_new.append([pos_new, None])
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                self.pop[idx] = self.get_better_solution(self.pop[idx], [pos_new, target])
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_wrapper_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new)
